2b9906bc9866e6d8616c4c809f237c5f9f9ef95c
thibd <thib.devineau@gmail.com>
2025-10-28 17:25:13 +0100

    fist page

--- IMMOSYNC/src/main/java/sio/immosync/controler/HelloController.java
+++ IMMOSYNC/src/main/java/sio/immosync/controler/HelloController.java
@@ -1,14 +1,41 @@
 package sio.immosync.controler;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
+    public ImageView btnEntrepreneur;
+    public ImageView btnInspecteur;
+    public ImageView btnProprio;
+    public ImageView btnInscription;
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void ClickEntreppreneur(MouseEvent mouseEvent) {
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        
+    }
+
+    public void ClickInspecteur(MouseEvent mouseEvent) {
+    }
+
+    public void ClickProprio(MouseEvent mouseEvent) {
+    }
+
+    public void ClickInscription(MouseEvent mouseEvent) {
     }
 }
